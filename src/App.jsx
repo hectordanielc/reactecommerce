56a6176dff4c87d116fa61bf7e768fdacf2d264d
Hector Castro <hectordanielcl@gmail.com>
2023-05-24 01:34:18 -0400

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import NavBar from './components/Navbar/NavBar'
+import ItemListContainer from './components/ItemListContainer/ItemListContainer'
+
+function App() {
+
+  return (
+  <div className="App">
+    <NavBar />
+    <ItemListContainer greeting="Bienvenido a la tienda" />
+  </div>
+  )
+}
+
+export default App
---
+++ src/components/CartWidget/CartWidget.jsx
@@ -0,0 +1,10 @@
+const CartWidget = () => {
+    return (
+        <div>
+            <img src="https://img.icons8.com/ios/50/000000/shopping-cart.png" alt="carrito" />
+            0
+        </div>
+    )
+}
+
+export default CartWidget
